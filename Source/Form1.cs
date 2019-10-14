81088c4f5e7f3af4943ea0964e48d6c19a4c0abc
Zalexanninev15 <51060911+Zalexanninev15@users.noreply.github.com>
2019-10-14 20:29:00 +0300

    Добавил исходный код

---
+++ Source/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Animations;
+using MaterialSkin.Controls;
+using MaterialSkin;
+
+namespace Translator
+{
+    public partial class Form1 : MaterialForm
+    {
+        Form f;
+        YandexTranslator yt;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            yt = new YandexTranslator();
+
+            // Дефолтная тема
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Red500, Primary.Red700, Primary.Red100, Accent.Blue200, TextShade.WHITE);
+            // 1 - под заголовком, 2 - заголовок, 3 - ?, 4 - элементы выбора
+
+        }
+
+        private void MaterialRadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B_w_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (b_w.Checked)
+            {
+                //Включение тёмной темы (галочка)
+                b_w.Text = "ТЁМНАЯ ТЕМА";
+                var materialSkinManager = MaterialSkinManager.Instance;
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue500, Primary.Blue700, Primary.Blue100, Accent.Yellow200, TextShade.WHITE);
+                //materialSkinManager.ColorScheme = new ColorScheme(Primary.Cyan500, Primary.Cyan700, Primary.Cyan100, Accent.Yellow200, TextShade.WHITE);
+            }
+            if (!b_w.Checked)
+            {
+                //Выключение тёмной темы (галочка) (включение дефолтной темы)
+                b_w.Text = "ТЁМНАЯ ТЕМА";
+                var materialSkinManager = MaterialSkinManager.Instance;
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+                //materialSkinManager.ColorScheme = new ColorScheme(Primary.Red600, Primary.Red700, Primary.Red400, Accent.Yellow700, TextShade.WHITE);
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.Red500, Primary.Red700, Primary.Red100, Accent.Blue200, TextShade.WHITE);
+            }
+        }
+
+        private void MaterialFlatButton1_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Clear();
+            outputTextBox.Clear();
+        }
+
+        private void MaterialFlatButton2_Click_1(object sender, EventArgs e)
+        {
+            f = new Form2();
+            f.Show();
+            b_w.Checked = false;
+        }
+
+        private void MaterialFlatButton4_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(outputTextBox.Text);
+        }
+
+        private void MaterialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            string lang;
+
+            if (materialRadioButton1.Checked == true)
+            {
+                lang = "ru-en";
+            }
+            else
+            {
+                lang = "en-ru";
+            }
+
+            outputTextBox.Text = yt.Translate(inputTextBox.Text, lang);
+        }
+
+        private void MaterialRaisedButton3_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(outputTextBox.Text);
+        }
+
+        private void MaterialRaisedButton4_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Clear();
+            outputTextBox.Clear();
+        }
+
+        private void MaterialRadioButton2_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaterialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            f = new Form2();
+            f.Show();
+            b_w.Checked = false;
+        }
+
+        private void MaterialRaisedButton5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://translate.yandex.ru");
+        }
+
+        private void materialRaisedButton6_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Text = Clipboard.GetText(); ;
+        }
+    }
+}
---
+++ Source/YandexTranslator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace Translator
+{
+    class YandexTranslator
+    {
+        public string Translate(string s, string lang)
+        {
+            if (s.Length > 0)
+            {
+                WebRequest request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr.json/translate?"
+                    + "key=trnsl.1.1.20191014T165237Z.d122eba3ce8bc4e1.65e9b9206a52de1471ee2170dcfe59e4800c7014"
+                    + "&text=" + s
+                    + "&lang=" + lang);
+
+                WebResponse response = request.GetResponse();
+
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    string line;
+
+                    if ((line = stream.ReadLine()) != null)
+                    {
+                        Translation translation = JsonConvert.DeserializeObject<Translation>(line);
+
+                        s = "";
+
+                        foreach (string str in translation.text)
+                        {
+                            s += str;
+                        }
+                    }
+                }
+
+                return s;
+            }
+            else
+                return "";
+        }
+    }
+
+    class Translation
+    {
+        public string code { get; set; }
+        public string lang { get; set; }
+        public string[] text { get; set; }
+    }
+}
