f92b911904fef9f8e7ee03838dde320cd859c4ae
Zalexanninev15 <blue.shark@disroot.org>
2020-07-21 21:22:05 +0300

    Update Form1.cs

--- Source/Form1.cs
+++ Source/Form1.cs
@@ -16,9 +16,94 @@ using Microsoft.Win32;
 namespace Translator
 {
     public partial class Form1 : MaterialForm
-    {
+    {[DllImport("user32", CharSet = CharSet.Auto)]
+        internal extern static bool PostMessage(IntPtr hWnd, uint Msg, uint WParam, uint LParam);
+        [DllImport("user32", CharSet = CharSet.Auto)]
+#pragma warning disable CS0108
+        internal extern static bool ReleaseCapture();
+#pragma warning restore CS0108
+        const uint WM_SYSCOMMAND = 0x0112;
+        const uint DOMOVE = 0xF012;
+        const uint DOSIZE = 0xF008;
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+        private bool m_aeroEnabled;
+        private const int CS_DROPSHADOW = 0x00020000;
+        private const int WM_NCPAINT = 0x0085;
+        private const int WM_ACTIVATEAPP = 0x001C;
+        [System.Runtime.InteropServices.DllImport("dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+        [System.Runtime.InteropServices.DllImport("dwmapi.dll")]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+        [System.Runtime.InteropServices.DllImport("dwmapi.dll")]
+        public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+        [System.Runtime.InteropServices.DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+        public struct MARGINS
+        {
+            public int leftWidth;
+            public int rightWidth;
+            public int topHeight;
+            public int bottomHeight;
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                m_aeroEnabled = CheckAeroEnabled();
+                CreateParams cp = base.CreateParams;
+                if (!m_aeroEnabled)
+                    cp.ClassStyle |= CS_DROPSHADOW; return cp;
+            }
+        }
+        private bool CheckAeroEnabled()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                int enabled = 0; DwmIsCompositionEnabled(ref enabled);
+                return (enabled == 1) ? true : false;
+            }
+            return false;
+        }
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCPAINT:
+                    if (m_aeroEnabled)
+                    {
+                        var v = 2;
+                        DwmSetWindowAttribute(this.Handle, 2, ref v, 4);
+                        MARGINS margins = new MARGINS()
+                        {
+                            bottomHeight = 1,
+                            leftWidth = 0,
+                            rightWidth = 0,
+                            topHeight = 0
+                        }; DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+                    }
+                    break;
+                default: break;
+            }
+            base.WndProc(ref m);
+        }
+
+        // =======================================================================================================================
+        // =======================================================================================================================
+        // =======================================================================================================================
+
+    
         Form f;
         string lang, theme;
+        int form2 = 0;
         YandexTranslator yt;
 
         public Form1()
@@ -168,8 +253,19 @@ namespace Translator
 
         private void MaterialRaisedButton2_Click(object sender, EventArgs e)
         {
-            f = new Form2();
-            f.Show();
+        foreach (Form f in Application.OpenForms)
+            {
+                if (f.Name == "Form2")
+                {
+                    form2 = 1;
+                }
+                else { form2 = 0; }
+            }
+            if (form2 == 0)
+            {
+                f = new Form2();
+                f.Show();
+            }
         }
 
         private void MaterialRaisedButton5_Click(object sender, EventArgs e)
